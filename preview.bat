da4dcbb8cf6b4416544cad753e03318d60312572
ox-carnage <ox_carnage@msn.com>
2021-02-09 14:39:22 -0300

    Atualizado

--- preview.bat
+++ preview.bat
@@ -1,2 +1,2 @@
-E:\Emuladores\Emuladores\Mega\Gens_r57shell_mod\Gens\gens.exe "vermilion_br.md"
+E:\Emuladores\Emuladores\Mega\Gens_r57shell_mod\Gens\gens.exe "bin\Sword of Vermillion (BR) [!] V3.0.bin"
 
